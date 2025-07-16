16c5fd38582a3ab3329d513b87159eb0bfb4ed2e
root <root@ip-10-68-64-73.us-east-2.compute.internal>
2025-07-16 04:18:42 +0000

    upload silicon mark creating job function

--- application/silicon_mark/test_silicon_mark.py
+++ application/silicon_mark/test_silicon_mark.py
@@ -1,23 +1,36 @@
+from time import sleep
 import pytest
+from pages.home_page import HomePage
+from pages.silicon_mark.silicon_mark_create_job_page import SiliconMarkCreateJobPage
 from pages.silicon_mark.silicon_mark_home_page import SiliconMarkHomePage
+from application.conftest import driver
+
+silicon_mark_page = SiliconMarkHomePage(driver)
 
 
-@pytest.mark.order(3)
-def test_navigate_to_silicon_mark(driver):
-    print(f"========== siliconmark ==> {driver.current_url}")
-    # silicon_mark_home_page = SiliconMarkHomePage(driver)
-    # silicon_mark_home_page.navigate_to_silicon_mark()
+@pytest.mark.order(1)
+def test_successful_login(driver):
+    driver.get("https://www.silicondata.com")
+    home_page = HomePage(driver)
+    print(f"current_page is {driver.current_url}")
+    login_page_object = home_page.navigate_to_login()
+    is_login, login_message = login_page_object.enter_credentials_and_direct_navigator("frank+protest_internal@silicondata.com", "qqq111!!")
+    assert is_login, login_message
 
-# @pytest.mark.order(4)
-# def test_all_mark_elements_exist(driver):
-#     """
-#     Confirm all necessary elements are present.
-#     :param driver:
-#     :return:
-#     """
-#     silicon_mark_home_page = SiliconMarkHomePage(driver)
-#     silicon_mark_home_page.navigate_to_silicon_mark()
-#     # silicon_mark_home_page
+@pytest.mark.order(2)
+def test_silicon_mark_page_elements_exist(driver):
+    window_size = driver.get_window_size()
+    print(window_size)
+    # return
+    print(f"test_silicon_mark_page_elements_exist=>{driver.current_url}")
+    silicon_mark_page = SiliconMarkHomePage(driver)
+    sleep(2)
+    print(f"==----------------=>{driver.current_url}")
+    silicon_mark_page.navigate_to_silicon_mark()
+    silicon_mark_page.silicon_mark_elements_exist()
 
+def test_create_job(driver):
+    print(f"test_create_job==> current_page is {driver.current_url}")
 
-
+    create_job_page_object = SiliconMarkCreateJobPage(driver)
+    create_job_page_object.create_job()
